aefb6c657ca1c78fa680dbe077b3bb00ac3460ba
underfr <loicklaurent28@gmail.com>
2026-04-17 09:27:21 +0200

    feat: add core html in index.php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/style/main.css">
+    <script src="./public/script/main.js" async></script>
+    <title>Accueil</title>
+</head>
+<body>
+    <h1>Bienvenue sur notre super site</h1>
+</body>
+</html>
